4e9ae33100f9dd3ad3ff8ba06ca76cad7144e635
Duong Van Tuy <tuydv@qsoft.com.vn>
2018-02-27 15:25:42 +0700

    n

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -143,4 +143,40 @@
 			return false;
 		});
 	});
+
+	// Quantity buttons
+	$('div.quantity:not(.buttons_added), td.quantity:not(.buttons_added)').addClass('buttons_added');
+	
+	$(document).on('click', '.plus, .minus', function() {
+		// Get values
+		var $qty		= $(this).closest('.quantity').find('.qty'),
+			currentVal	= parseFloat($qty.val()),
+			max			= '',
+			min			= 1,
+			step		= 1;
+
+		// Format values
+		if(! currentVal || currentVal === '' || currentVal === 'NaN') currentVal = 0;
+		if(max === '' || max === 'NaN') max = '';
+		if(min === '' || min === 'NaN') min = 0;
+		if(step === 'any' || step === '' || step === undefined || parseFloat(step) === 'NaN') step = 1;
+
+		// Change the value
+		if($(this).is('.plus')) {
+			if(max &&(max == currentVal || currentVal > max)) {
+				$qty.val(max);
+			} else {
+				$qty.val(currentVal + parseFloat(step));
+			}
+		} else {
+			if(min &&(min == currentVal || currentVal < min)) {
+				$qty.val(min);
+			} else if(currentVal > 0) {
+				$qty.val(currentVal - parseFloat(step));
+			}
+		}
+
+		// Trigger change event
+		$qty.trigger('change');
+	});
 })(jQuery);
